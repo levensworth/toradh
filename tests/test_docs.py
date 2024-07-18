664c3a321c7c0578e5effcc46adcc0f7101470d4
Santiago Bassani <santiago.bassani96@gmail.com>
2024-07-18 14:30:54 +0100

    [ADD] skip test docs for lower versions

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,14 +1,16 @@
 import pathlib
+import sys
 from mktestdocs import check_md_file  # type: ignore
 import pytest
 
 
 # Note the use of `str`, makes for pretty output
+@pytest.mark.skipif(sys.version_info < (3, 10), reason="requires Python 3.10 or higher")
 @pytest.mark.parametrize("fpath", pathlib.Path("docs").glob("**/*.md"), ids=str)
 def test_docs(fpath):
     check_md_file(fpath=fpath)
 
-
+@pytest.mark.skipif(sys.version_info < (3, 10), reason="requires Python 3.10 or higher")
 @pytest.mark.parametrize(
     "fpath",
     pathlib.Path(".").glob("readme.md"),
